dc8eae842102c6742a1b791ff83df698bf0bcf34
brunocs90@gmail.com <brunocs90@gmail.com>
2024-02-02 16:41:28 -0300

    feat: welcome message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'Barbearia',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} dark`}>{children}</body>
+      <body className={`${nunito.className} dark`}>{children}</body>
     </html>
   );
 }
